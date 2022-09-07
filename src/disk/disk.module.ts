86c036ff3c9c052f76559e15c053e6264aff52ba
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-07 16:32:47 +0200

    Setting up all my dependencies

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
 import { DiskService } from './disk.service';
 
+
+
 @Module({
-  providers: [DiskService]
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
+  
 })
 export class DiskModule {}
--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
+/**
+ * NB : without "exports" property, the 'PowerService' is only accessible 
+ * in the 'PowerModule'
+ * With "exports" property, the 'PowerService' is accessible by any modules
+ */
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService] 
 })
 export class PowerModule {}
